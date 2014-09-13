c870e87e3db573ba13c11b9261ab3a261d2b1144
tamakiii <tamaki.htm@gmail.com>
2014-09-14 08:51:10 +0900

    add Pdo\States\ReadInterface

---
+++ src/Concretes/Pdo/States/ReadInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Concretehouse\Dp\Repository\Concretes\Pdo\States;
+
+use Concretehouse\Dp\Repository\Concretes\Pdo;
+
+/**
+ * Pdo read state interface.
+ */
+interface ReadInterface extends Pdo\StateInterface
+{
+}
